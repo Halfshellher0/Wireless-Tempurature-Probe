1f2a0c75f80ce0279fdba3897858fdca5521b9f6
Matt Turinski <matt.turinski@gmail.com>
2020-10-17 22:45:55 -0400

    Initial Commit

---
+++ ESP Arduino Code/ESP-Client/Tempurature-Sensor.ino
@@ -0,0 +1,52 @@
+/********************************************************************/
+// First we include the libraries
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 21 
+#define SleepSeconds 60
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+void setup(void) 
+{ 
+ // start serial port and sensors
+ Serial.flush();
+ Serial.begin(115200);
+ Serial.flush();
+ sensors.begin();
+ delay(1000);
+
+ //Get Tempurature
+ float t1, t2, t3, t4, t;
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ t1 = sensors.getTempCByIndex(0);
+ delay(10);
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ t2 = sensors.getTempCByIndex(0);
+ delay(10);
+  sensors.requestTemperatures(); // Send the command to get temperature readings 
+ t3 = sensors.getTempCByIndex(0);
+ delay(10);
+  sensors.requestTemperatures(); // Send the command to get temperature readings 
+ t4 = sensors.getTempCByIndex(0);
+ delay(10);
+ t = (t1 + t2 + t3 + t4) / 4;
+ Serial.println(t);
+
+ //Go to Deep Sleep
+ Serial.flush();
+ Serial.end();
+ esp_sleep_enable_timer_wakeup(SleepSeconds * 1000000);
+ esp_deep_sleep_start(); 
+} 
+void loop(void) 
+{ 
+  //
+} 
---
+++ ESP-Server/ESP-Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ESP_Server
+{
+    class Program
+    {
+        static private void StartServer()
+        {
+            TcpListener server = new TcpListener(System.Net.IPAddress.Any, 8888);
+            //Start the server
+            server.Start();
+
+            Console.WriteLine("Server started. Waiting for connection...");
+            //Block execution until a new client is connected.
+            TcpClient newClient = server.AcceptTcpClient();
+
+            Console.WriteLine("New client connected!");
+
+            //Checking if new data is available to be read on the network stream
+            if (newClient.Available > 0)
+            {
+                //Initializing a new byte array the size of the available bytes on the network stream
+                byte[] readBytes = new byte[newClient.Available];
+                //Reading data from the stream
+                newClient.GetStream().Read(readBytes, 0, newClient.Available);
+                //Converting the byte array to string
+                String str = System.Text.Encoding.ASCII.GetString(readBytes);
+                //This should output "Hello world" to the console window
+                Console.WriteLine(str);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartServer();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
